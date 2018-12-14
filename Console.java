68047e25d40781adcf4682ac75874950b45623d8
Telesto79 <44401347+Telesto79@users.noreply.github.com>
2018-12-14 09:21:40 +0100

    Update Console.java

--- Console.java
+++ Console.java
@@ -2,66 +2,68 @@ package ProjetJava;
 
 import java.util.Scanner;
 
-public class Console extends Votant {
+public class Console extends Vote {
 	
-	public static int Nombre() {
+	public static int NombredeVotants() {
 		System.out.println("Combien de votants?");
 		Scanner scanner = new Scanner(System.in);
 		int nombre = scanner.nextInt();
 		return nombre;
 	}
 	
-	public static int Nombre2() {
+	public static int NombredeParticipants() {
 		System.out.println("Combien de participants?");
 		Scanner scanner = new Scanner(System.in);
 		int nombre2 = scanner.nextInt();
-		return nombre2;
+			return nombre2;
 	}
 	
 	public static void main(String [] args) {
-		int participants[] = new int[4];
-		int maxVal = Integer.MAX_VALUE;
-		participants[0] = 0;
-		participants[1] = 0;
-		participants[2] = 0;
-		participants[3] = 0;
-		int nombre = Nombre();
-		//int nombre2 = Nombre2();
-		int Vote[] = new int[nombre];
 		
-		for(int i=0;i<nombre;i++) {
+		int maxVal = Integer.MAX_VALUE;
+		int nombredeVotants = NombredeVotants();
+		int nombredeParticipants =NombredeParticipants();
+		int participants[] = new int[nombredeParticipants];
+		int Vote[] = new int[nombredeVotants];
+		String Part[]= new String[nombredeParticipants];
+		String str = "";
+		
+		for(int i=0;i<nombredeParticipants;i++) {
+			participants[i]= 0;
+			System.out.println("Quel est le nom du participant?");
+			Scanner scanner = new Scanner(System.in);
+			String part = scanner.nextLine();
+			Part[i] = part;
+			str +="  " +(i+1)+")"+Part[i];
+		}
+		
+		for(int i=0;i<nombredeVotants;i++) {
 			
-			System.out.println("Quel est ton vote?(1.2.3.4)");
+			System.out.println("Quel est ton vote?" + str);
 			
 			Scanner scanner = new Scanner(System.in);
 			int vot = scanner.nextInt();
 			System.out.println("Tu as voté pour " + vot);
 			Vote[i] = vot;
 		}
-		for (int i=0; i<nombre;i++) {
-			if (Vote[i] == 1) {
-				participants[0] +=1;
-			}
-			if (Vote[i] == 2) {
-				participants[1] +=1;
-			}
-			if (Vote[i] == 3) {
-				participants[2] +=1;
-			}
-			if (Vote[i] == 4) {
-				participants[3] +=1;
-			}
-			
-		}
-		System.out.println("Participant 1 "+(participants[0]*100)/nombre+ "%");
-		System.out.println("Participant 2 "+(participants[1]*100)/nombre+ "%");
-		System.out.println("Participant 3 "+(participants[2]*100)/nombre+ "%");
-		System.out.println("Participant 4 "+(participants[3]*100)/nombre+ "%");
 		
-		for(int i = 0; i < nombre; i++){
+		for (int i=0; i<nombredeParticipants;i++) {
+			for(int j=0;j<nombredeVotants;j++) {
+				if(Vote[j]==(i+1)) {
+					participants[i]+=1;
+				}
+			}
+		}
+		
+		for(int i=0;i<nombredeParticipants;i++) {
+		System.out.println("Participant "+ (i+1)  +" " +(participants[i]*100)/nombredeVotants+ "%");
+		}
+		
+		for(int i = 0; i < nombredeVotants; i++){
 	         if(Vote[i] < maxVal)
 	           maxVal = Vote[i];
 		}
-		System.out.println("Le gagnant est le candidat numéro "+ maxVal );
+		
+		System.out.println("Le gagnant est le candidat numéro "+ maxVal + " : "+Part[(maxVal-1)]);
 	 }
 	}
