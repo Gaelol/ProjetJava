72a03767ebb5e0c980a05504568f3006416297b0
Gaelol <37298427+Gaelol@users.noreply.github.com>
2018-12-19 16:00:36 +0100

    Update Vote.sql

--- Vote.sql
+++ Vote.sql
@@ -1,9 +1,15 @@
+CREATE SCHEMA `demo` ;
+
 CREATE  TABLE `demo`.`Vote` (
   `Nom` VARCHAR(45) NULL ,
   `Num` INT NULL ,
   `Pourc` INT NULL );
 
-SET @@global.time_zone = '+00:00'
-SET @@session.time_zone = '+00:00'
 
+CREATE  TABLE `demo`.`Votant` (
+  `Nom` VARCHAR(45) NULL ,
+  `Vote` INT NULL ,
+  `Age` INT NULL );
 
+SET @@global.time_zone = '+00:00';
+SET @@session.time_zone = '+00:00';
