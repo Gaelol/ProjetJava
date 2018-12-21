11919246330fa6c15d09bd2e62382b1381407051
Telesto79 <44401347+Telesto79@users.noreply.github.com>
2018-12-21 11:19:20 +0100

    Update VoteVueConsole.java

--- VoteVueConsole.java
+++ VoteVueConsole.java
@@ -1,5 +1,7 @@
 package ProjetJava;
-
+/**
+ * @author De Mal Raphaël, Dieuzeide Gaël
+ */
 import java.util.InputMismatchException;
 import java.util.Observable;
 import java.util.Observer;
@@ -28,136 +30,127 @@ public class VoteVueConsole extends VoteVue implements Observer {
 		System.out.println(model);
 	}
 
-	/**
-	 * @author gael-
-	 *
-	 */
 	private class ReadInput implements Runnable {
 		public void run() {
-	
+
 			while (true) {
-					try {
-						affiche("Combien de votants?");
-						int c = sc.nextInt();
-						controller.nombredeVotants(c);
-						break;
-					} catch (InputMismatchException e) {
-						System.err.println("Entrez un nombre s'il vous plait!");
-						sc.next();
-						continue;
-					}
-				}
-			
-					
-				while (true) {
-					try {
-						System.out.println("Combien de participants?");
-						int c = sc.nextInt();
-						controller.nombredeParticipants(c);
-						break;
-					} catch (InputMismatchException e) {
-						System.err.println("Entrez un nombre s'il vous plait!");
-						sc.next();
-						continue;
-					}
-				}
-
-				while (true) {
-					try {
-						System.out.println("Quel type de vote? 'Simple(1)' ou 'Detaille(2)'?");
-						int c = sc.nextInt();
-						controller.choix(c);
-						break;
-					} catch (InputMismatchException e) {
-						System.err.println("Entrez 'Simple(1)' ou 'Detaille(2)' s'il vous plait!");
-						sc.next();
-						continue;
-					}
-				}
-				
-				
-				switch (controller.choix) {
-
-				case 1:
-
-					controller.reinitialiserDb();
-
-			
-					for (int i = 0; i < controller.nombredeParticipants; i++) {
-						
-						System.out.println("Quel est le nom du participant numero " + (i + 1) + "?");
-						String c = sc.next();
-						controller.nomParticipants(c,i);
-						
-					}
-
-					
-			
-					for (int i = 0; i < controller.nombredeVotants; i++) {
-						while (true) {
-							try {
-								affiche("Quel est ton vote?(Si le vote est un mauvais nombre: Vote blanc)\n"+controller.str);
-								int c = sc.nextInt();
-								controller.vote(c,i);
-								break;
-							} catch (InputMismatchException e) {
-								System.err.println("Entrez un nombre s'il vous plait!\n");
-								sc.next();
-								continue;
-							}
-						}
-					}
-					
-					controller.depouillage();
-					controller.pourcentages();
-					controller.gagnant();
+				try {
+					affiche("Combien de votants?");
+					int c = sc.nextInt();
+					controller.nombredeVotants(c);
 					break;
+				} catch (InputMismatchException e) {
+					System.err.println("Entrez un nombre s'il vous plait!");
+					sc.next();
+					continue;
+				}
+			}
 
-				case 2:
+			while (true) {
+				try {
+					System.out.println("Combien de participants?");
+					int c = sc.nextInt();
+					controller.nombredeParticipants(c);
+					break;
+				} catch (InputMismatchException e) {
+					System.err.println("Entrez un nombre s'il vous plait!");
+					sc.next();
+					continue;
+				}
+			}
+
+			while (true) {
+				try {
+					System.out.println("Quel type de vote? 'Simple(1)' ou 'Detaille(2)'?");
+					int c = sc.nextInt();
+					controller.choix(c);
+					break;
+				} catch (InputMismatchException e) {
+					System.err.println("Entrez 'Simple(1)' ou 'Detaille(2)' s'il vous plait!");
+					sc.next();
+					continue;
+				}
+			}
+
+			switch (controller.choix) {
+
+			case 1:
 
 				controller.reinitialiserDb();
 
 				for (int i = 0; i < controller.nombredeParticipants; i++) {
-					
-					affiche("Quel est le nom du participant numero " + (i + 1) + "?");
+
+					System.out.println("Quel est le nom du participant numero " + (i + 1) + "?");
 					String c = sc.next();
-					controller.nomParticipants(c,i);
-					
+					controller.nomParticipants(c, i);
+
 				}
-				
-					for (int i = 0; i < controller.nombredeVotants; i++) {
-						
-						
-						while (true) {
-							try {
-								affiche("Comment t'appelles-tu?");
-								String c = sc.next();
-								affiche("Quel âge as-tu?");
-								int k = sc.nextInt();
-								affiche("Quel est ton vote?(Si le vote est un mauvais nombre: Vote blanc)\n"+controller.str);
-								int j = sc.nextInt();
-								controller.votantComplet(c,k,j,i);
-								break;
-							} catch (InputMismatchException e) {
-								System.err.println("Entrez un nombre s'il vous plait!");
-								sc.next();
-								continue;
-							}
+
+				for (int i = 0; i < controller.nombredeVotants; i++) {
+					while (true) {
+						try {
+							affiche("Quel est ton vote?(Si le vote est un mauvais nombre: Vote blanc)\n"
+									+ controller.str);
+							int c = sc.nextInt();
+							controller.vote(c, i);
+							break;
+						} catch (InputMismatchException e) {
+							System.err.println("Entrez un nombre s'il vous plait!\n");
+							sc.next();
+							continue;
 						}
 					}
+				}
 
-					
+				controller.depouillage();
+				controller.pourcentages();
+				controller.gagnant();
+				break;
+
+			case 2:
+
+				controller.reinitialiserDb();
+
+				for (int i = 0; i < controller.nombredeParticipants; i++) {
+
+					affiche("Quel est le nom du participant numero " + (i + 1) + "?");
+					String c = sc.next();
+					controller.nomParticipants(c, i);
+
+				}
+
+				for (int i = 0; i < controller.nombredeVotants; i++) {
+
+					while (true) {
+						try {
+							affiche("Comment t'appelles-tu?");
+							String c = sc.next();
+							affiche("Quel Ã¢ge as-tu?");
+							int k = sc.nextInt();
+							affiche("Quel est ton vote?(Si le vote est un mauvais nombre: Vote blanc)\n"
+									+ controller.str);
+							int j = sc.nextInt();
+							controller.votantComplet(c, k, j, i);
+							break;
+						} catch (InputMismatchException e) {
+							System.err.println("Entrez un nombre s'il vous plait!");
+							sc.next();
+							continue;
+						}
+					}
+				}
+
 				controller.depouillage();
 				controller.pourcentagesDetail();
 				break;
 
-				}
-				}
+			}
+		}
 	}
 
 	@Override
 	public void affiche(String string) {
 		System.out.println(string);
-		
+
 	}
 }
