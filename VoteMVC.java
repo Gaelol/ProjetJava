ffc5649279c560ffdf3c100ef178f42c59c850b9
Gaelol <37298427+Gaelol@users.noreply.github.com>
2018-12-21 17:13:23 +0100

    Update VoteMVC.java

--- VoteMVC.java
+++ VoteMVC.java
@@ -1,33 +1,49 @@
-package ProjetJava;
+package ProjetJava.test;
+
+import ProjetJava.controller.VoteController;
+import ProjetJava.model.Vote;
+import ProjetJava.view.VoteVueConsole;
+import ProjetJava.view.VoteVueGUI;
+
 /**
  * @author De Mal Raphaël, Dieuzeide Gaël
  */
 public class VoteMVC {
 	public VoteMVC() {
-		// CrÃƒÂ©ation du modÃƒÂ¨le
+		/**
+		 * model creation
+		 */
 		Vote model = new Vote();
-
-		// CrÃƒÂ©ation des contrÃƒÂ´leurs : Un pour chaque vue
-		// Chaque contrÃƒÂ´leur doit avoir une rÃƒÂ©fÃƒÂ©rence vers le modÃƒÂ¨le pour
-		// pouvoir le commander
+		
+		/**
+		 * Create controller : one for each vue
+		 * Each controller needs a reference to the model for commanding him
+		 */
+	
 		VoteController consoleController = new VoteController(model);
 		VoteController guiController = new VoteController(model);
-
-		// CrÃƒÂ©ation des vues.
-		// Chaque vue doit connaÃƒÂ®tre son contrÃƒÂ´leur et avoir une rÃƒÂ©fÃƒÂ©rence
-		// vers le modÃƒÂ¨le pour pouvoir l'observer
+		
+		/**
+		 * view creation
+		 * each view need to know the controler and to have a reference to model for watching him
+		 */
+		
 		VoteVueConsole console = new VoteVueConsole(model, consoleController);
 		VoteVueGUI gui = new VoteVueGUI(model, guiController, 0, 0);
 
-		// On donne la rÃƒÂ©fÃƒÂ©rence ÃƒÂ  la vue pour chaque contrÃƒÂ´leur
+		/**
+		 * give reference and vue to the controller
+		 */
 		guiController.addView(gui);
 		consoleController.addView(console);
 
 	}
 
 	public static void main(String args[]) {
-		// Schedule a job for the event-dispatching thread:
-		// creating and showing this application's GUI.
+		/**
+		 * Schedule a job for the event-dispatching thread:
+		 * creating and showing this application's GUI.
+		 */
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				new VoteMVC();
