0cbf6ed8ad843233016a9772f80feafb984e9ac0
Telesto79 <44401347+Telesto79@users.noreply.github.com>
2018-12-21 11:17:26 +0100

    Update VoteController.java

--- VoteController.java
+++ VoteController.java
@@ -1,5 +1,8 @@
 package ProjetJava;
 
+/**
+ * @author De Mal Raphaël, Dieuzeide Gaël
+ */
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -8,53 +11,123 @@ import java.sql.Statement;
 public class VoteController {
 	Vote model;
 	VoteVue vue;
+	/**
+	 * the amount of people able to vote
+	 */
 	int nombredeVotants = 0;
+	/**
+	 * the amount of people you can vote for
+	 */
 	int nombredeParticipants = 0;
+	/**
+	 * the choice the user makes when creating his election when chosing between
+	 * 'simple'(1) or 'détaillé'(2)
+	 */
 	int choix = 0;
+	/**
+	 * the url used to acces the database containing all the date of the election
+	 * system created
+	 */
 	String url = "jdbc:mysql://localhost:3306/demo";
+	/**
+	 * the username used to log in the database
+	 */
 	String user = "root";
+	/**
+	 * the password used to log in the database
+	 */
 	String password = "password";
+	/**
+	 * TODO
+	 */
 	Connection myConn = null;
+	/**
+	 * TODO
+	 */
 	Statement myStmt = null;
+	/**
+	 * TODO
+	 */
 	ResultSet myRs;
+	/**
+	 * the maximum value used to determin who won the elections
+	 */
 	int maxVal = Integer.MAX_VALUE;
+	/**
+	 * the String used to show the name of the person to be elected with his id
+	 */
 	String str = " ";
+	/**
+	 * the name of the person that just voted
+	 */
 	String nom = " ";
+	/**
+	 * the age of the person that just voted
+	 */
 	int age = 0;
+	/**
+	 * TODO
+	 */
 	int vote = 8524;
-	
-	
+
+	/**
+	 * TODO
+	 */
 	public VoteController(Vote model) {
 		this.model = model;
 	}
-	
+
+	/**
+	 * TODO
+	 */
 	public void addView(VoteVue vue) {
 		this.vue = vue;
-		
 	}
 
-	
+	/**
+	 * sets the amount of people who can vote 
+	 * @param Votants the amount entered by the user
+	 */
 	public void nombredeVotants(int Votants) {
 		this.nombredeVotants = Votants;
-	
 	}
-	
+
+	/**
+	 * sets the amount of people you can vote for 
+	 * @param Participants the amount entered by the user
+	 */
 	public void nombredeParticipants(int Participants) {
 		this.nombredeParticipants = Participants;
 	}
-	
+
+	/**
+	 * sets the choice the user made when choosing between 'simple' mode or 'détaillé'
+	 * @param c the choice the user made (1->'simple' and 2->'detaillé')
+	 */
 	public void choix(int c) {
 		this.choix = c;
 	}
-	int participants[] = new int[1000];//numero
-	int Vote[] = new int[1000];//vote
-	String nomdesPart[] = new String[1000];//nom
-	
 
+	/**
+	 * an array of ints of size 1000 where each component is someone you can vote for
+	 */
+	int participants[] = new int[1000];// numero
+	/**
+	 * an array of ints of size 1000 where each component is a vote 
+	 */
+	int Vote[] = new int[1000];// vote
+	/**
+	 * 
+	 */
+	String nomdesPart[] = new String[1000];// nom
+
+	/**
+	 * 
+	 */
 	public void nomParticipants(String nom, int i) {
-			str += "  " + (i + 1) + ")" + nom;
-			participants[i] = 0;
-			nomdesPart[i] =nom;
+		str += "  " + (i + 1) + ")" + nom;
+		participants[i] = 0;
+		nomdesPart[i] = nom;
 		try {
 			myConn = DriverManager.getConnection(url, user, password);
 			myStmt = myConn.createStatement();
@@ -63,16 +136,21 @@ public class VoteController {
 		} catch (Exception exc) {
 			exc.printStackTrace();
 		}
-		}
-	
-	
-	public void vote(int vote,int i) {
-		
-			Vote[i]=vote;
-			vue.affiche("Tu as vote pour " + vote);
-		
 	}
-	
+
+	/**
+	 * 
+	 */
+	public void vote(int vote, int i) {
+
+		Vote[i] = vote;
+		vue.affiche("Tu as vote pour " + vote);
+
+	}
+
+	/**
+	 * 
+	 */
 	public void depouillage() {
 		for (int i = 0; i < nombredeParticipants; i++) {
 			for (int j = 0; j < nombredeVotants; j++) {
@@ -82,7 +160,10 @@ public class VoteController {
 			}
 		}
 	}
-	
+
+	/**
+	 * 
+	 */
 	public void pourcentages() {
 		for (int i = 0; i < nombredeParticipants; i++) {
 			int prc = (participants[i] * 100) / nombredeVotants;
@@ -97,7 +178,10 @@ public class VoteController {
 			vue.affiche("Participant " + (i + 1) + " " + (participants[i] * 100) / nombredeVotants + "%");
 		}
 	}
-	
+
+	/**
+	 * 
+	 */
 	public void pourcentagesDetail() {
 		for (int i = 0; i < nombredeParticipants; i++) {
 			int prc = (participants[i] * 100) / nombredeVotants;
@@ -126,44 +210,49 @@ public class VoteController {
 		} catch (Exception exc) {
 			exc.printStackTrace();
 		}
-		
-		
-		
+
 	}
-	
+
+	/**
+	 * 
+	 */
 	public void gagnant() {
 		for (int i = 0; i < nombredeVotants; i++) {
-			
+
 			if (Vote[i] == maxVal)
-			vue.affiche("Egalite entre:"+nomdesPart[(maxVal - 1)]+" et "+nomdesPart[Vote[i]-1]);
-			
-			
+				vue.affiche("Egalite entre:" + nomdesPart[(maxVal - 1)] + " et " + nomdesPart[Vote[i] - 1]);
+
 			if (Vote[i] < maxVal)
 				maxVal = Vote[i];
 			vue.affiche("Le gagnant est le candidat numero " + maxVal + " : " + nomdesPart[(maxVal - 1)]);
 			break;
 		}
-		
+
 	}
-	
-	public void votantComplet(String nom,int age,int vot,int i) {
-		this.nom=nom;
+
+	/**
+	 * 
+	 */
+	public void votantComplet(String nom, int age, int vot, int i) {
+		this.nom = nom;
 		this.age = age;
-		Vote[i]=vot;
-		
+		Vote[i] = vot;
+
 		try {
 			myConn = DriverManager.getConnection(url, user, password);
 			myStmt = myConn.createStatement();
 
-			String sql = "insert into Votant " + " (Nom,Vote,Age)" + " values ('" + nom + "','" + vot
-					+ "','" + age + "')";
+			String sql = "insert into Votant " + " (Nom,Vote,Age)" + " values ('" + nom + "','" + vot + "','" + age
+					+ "')";
 			myStmt.executeUpdate(sql);
 		} catch (Exception exc) {
 			exc.printStackTrace();
 		}
 	}
-	
-	
+
+	/**
+	 * 
+	 */
 	public void reinitialiserDb() {
 		try {
 			myConn = DriverManager.getConnection(url, user, password);
@@ -177,8 +266,4 @@ public class VoteController {
 		}
 	}
 
-	
-	
-
-	
 }
