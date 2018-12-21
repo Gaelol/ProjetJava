c4ba75d74dc6c6a3e36a54fcc5bd5176ecc96922
Telesto79 <44401347+Telesto79@users.noreply.github.com>
2018-12-21 11:19:52 +0100

    Update VoteVue.java

--- VoteVue.java
+++ VoteVue.java
@@ -1,21 +1,19 @@
 package ProjetJava;
-
+/**
+ * @author De Mal Raphaël, Dieuzeide Gaël
+ */
 import java.util.Observer;
 
-import ProjetJava.VoteController;
-import ProjetJava.Vote;
+public abstract class VoteVue implements Observer {
 
-public abstract class VoteVue implements Observer{
-	
 	protected Vote model;
 	protected VoteController controller;
-	
-	VoteVue(Vote model,
-			VoteController controller) {
+
+	VoteVue(Vote model, VoteController controller) {
 		this.model = model;
 		this.controller = controller;
 		model.addObserver(this);
 	}
 
-	public abstract void affiche(String string) ;
+	public abstract void affiche(String string);
 }
