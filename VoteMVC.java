f86d4e82c243e8acf3d677939dbc7e878cb196bc
Telesto79 <44401347+Telesto79@users.noreply.github.com>
2018-12-21 11:20:14 +0100

    Update VoteMVC.java

--- VoteMVC.java
+++ VoteMVC.java
@@ -1,44 +1,33 @@
 package ProjetJava;
-
-import ProjetJava.VoteController;
-import ProjetJava.Vote;
-import ProjetJava.VoteMVC;
-import ProjetJava.VoteVue;
-import ProjetJava.VoteVueConsole;
-import ProjetJava.VoteVueGUI;
-
+/**
+ * @author De Mal Raphaël, Dieuzeide Gaël
+ */
 public class VoteMVC {
 	public VoteMVC() {
-		//CrÃ©ation du modÃ¨le
+		// CrÃƒÂ©ation du modÃƒÂ¨le
 		Vote model = new Vote();
-		//TODO
 
-		//CrÃ©ation des contrÃ´leurs : Un pour chaque vue
-		//Chaque contrÃ´leur doit avoir une rÃ©fÃ©rence vers le modÃ¨le pour pouvoir le commander
+		// CrÃƒÂ©ation des contrÃƒÂ´leurs : Un pour chaque vue
+		// Chaque contrÃƒÂ´leur doit avoir une rÃƒÂ©fÃƒÂ©rence vers le modÃƒÂ¨le pour
+		// pouvoir le commander
 		VoteController consoleController = new VoteController(model);
 		VoteController guiController = new VoteController(model);
-		
-		
-		
-		//CrÃ©ation des vues.
-		//Chaque vue doit connaÃ®tre son contrÃ´leur et avoir une rÃ©fÃ©rence vers le modÃ¨le pour pouvoir l'observer
-		VoteVueConsole console = new VoteVueConsole(model,consoleController);
-		VoteVueGUI gui = new VoteVueGUI(model,guiController,0,0);
-		
-		//TODO
-		
-		//On donne la rÃ©fÃ©rence Ã  la vue pour chaque contrÃ´leur
+
+		// CrÃƒÂ©ation des vues.
+		// Chaque vue doit connaÃƒÂ®tre son contrÃƒÂ´leur et avoir une rÃƒÂ©fÃƒÂ©rence
+		// vers le modÃƒÂ¨le pour pouvoir l'observer
+		VoteVueConsole console = new VoteVueConsole(model, consoleController);
+		VoteVueGUI gui = new VoteVueGUI(model, guiController, 0, 0);
+
+		// On donne la rÃƒÂ©fÃƒÂ©rence ÃƒÂ  la vue pour chaque contrÃƒÂ´leur
 		guiController.addView(gui);
 		consoleController.addView(console);
-		
-		//TODO
-		
-		
+
 	}
 
 	public static void main(String args[]) {
-		//Schedule a job for the event-dispatching thread:
-		//creating and showing this application's GUI.
+		// Schedule a job for the event-dispatching thread:
+		// creating and showing this application's GUI.
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				new VoteMVC();
@@ -46,4 +35,3 @@ public class VoteMVC {
 		});
 	}
 }
-
