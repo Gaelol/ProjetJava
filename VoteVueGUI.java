63a77b6b04d135e16b2316ebcab7ee6a00cb522d
Telesto79 <44401347+Telesto79@users.noreply.github.com>
2018-12-21 16:18:45 +0100

    Update VoteVueGUI.java

--- VoteVueGUI.java
+++ VoteVueGUI.java
@@ -22,6 +22,17 @@ public class VoteVueGUI extends VoteVue {
 	private JButton submit;
 	private int phase = 0;
 	private JButton valider;
+	private int numeroCandidat = 1;
+
+	public static boolean isParsable(String input) {
+		boolean parsable = true;
+		try {
+			Integer.parseInt(input);
+		} catch (NumberFormatException e) {
+			parsable = false;
+		}
+		return parsable;
+	}
 
 	public VoteVueGUI(Vote model, VoteController guiController, int posX, int posY) {
 
@@ -31,93 +42,89 @@ public class VoteVueGUI extends VoteVue {
 		voteJFrame = new JFrame("vote MVC");
 		voteJFrame.setVisible(true);
 		voteJFrame.setResizable(false);
-		voteJFrame.setBounds(500,90, 500, 90);
+		voteJFrame.setBounds(600, 90, 600, 90);
 		voteJFrame.setFocusTraversalKeysEnabled(false);
 		voteJFrame.setTitle("Cr\u00E9ateur de votes");
 		voteJFrame.getContentPane().setLayout(null);
 		zoneQuestion.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		zoneQuestion.setHorizontalAlignment(SwingConstants.CENTER);
-		zoneQuestion.setBounds(0, 0, 348, 59);
+		zoneQuestion.setBounds(0, 0, 449, 59);
 		voteJFrame.getContentPane().add(zoneQuestion);
-		zoneQuestion.setText("Combien de Votants?");
+		zoneQuestion.setText("Combien de Votants y a-t-il?");
 
 		textField = new JTextField();
-		textField.setBounds(349, 24, 145, 35);
+		textField.setBounds(449, 24, 145, 35);
 		voteJFrame.getContentPane().add(textField);
 		textField.setColumns(10);
-		
-		valider = new JButton("New button");
+
+		valider = new JButton("Valider");
 		valider.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent f) {
 				switch (phase) {
 				case 0:
-					while (true) {
-						try {
-							affiche("Combien de votants?");
-							int c = Integer.parseInt(textField.getText());
-							controller.nombredeVotants(c);
-							break;
-						} catch (InputMismatchException e) {
-							affiche("Entrez un nombre s'il vous plait!");
-							continue;
-						}
+					controller.reinitialiserDb();
+					if (isParsable(textField.getText())) {
+						int c = Integer.parseInt(textField.getText());
+						controller.nombredeVotants(c);
+						phase++;
+						affiche("Combien de participants?");
+					} else {
+						affiche("Vous devez entrer un nombre!");
 					}
-					phase++;
 					break;
 
 				case 1:
-					while (true) {
-						try {
-							affiche("Combien de participants?");
-							int c = Integer.parseInt(textField.getText());
-							controller.nombredeParticipants(c);
-							break;
-						} catch (InputMismatchException e) {
-							affiche("Entrez un nombre s'il vous plait!");
-							continue;
-						}
+					if (isParsable(textField.getText())) {
+						int g = Integer.parseInt(textField.getText());
+						controller.nombredeParticipants(g);
+						affiche("Quel type de vote? Pour simple, tapez '1' ou pour detaillé tapez '2' ?");
+						phase++;
+					} else {
+						affiche("Vous devez entrer un nombre!");
 					}
-					phase++;
 					break;
-
 				case 2:
-					while (true) {
-						try {
-							affiche("Quel type de vote? Pour simple, tapez '1' ou pour Detaillé tapez '2' ?");
-							int c = Integer.parseInt(textField.getText());
-							controller.choix(c);
-							break;
-						} catch (InputMismatchException e) {
+					if (isParsable(textField.getText())) {
+						int x = Integer.parseInt(textField.getText());
+						if (x != 1 && x != 2) {
 							affiche("Entrez '1'(Simple) ou '2'(Detaillé) s'il vous plait!");
-							continue;
+						} else {
+							controller.choix(x);
 						}
+					} else {
+						affiche("Vous devez entrer un nombre!");
 					}
+					affiche("Quel est le nom du participant numero 1?");
 					phase++;
 					break;
 
 				case 3:
-					for (int i = 0; i < controller.nombredeParticipants; i++) {
-
-						affiche("Quel est le nom du participant numero " + (i + 1) + "?");
-						String c = textField.getText();
-						controller.nomParticipants(c, i);
-
+					String z = textField.getText();
+					controller.nomParticipants(z, numeroCandidat - 1);
+					affiche("Quel est le nom du participant numero " + (numeroCandidat + 1) + "?");
+					numeroCandidat++;
+					if (numeroCandidat - 1 >= controller.nombredeParticipants) {
+						if (controller.choix == 1) {
+							affiche("Quel est ton vote?" + controller.str
+									+ ". Veuillez entrer le numéro correspondant à la personne pour laquelle vous désirez voter.");
+						} else {
+							affiche("Donne ton nom,ton age et ton vote espacé, ex:'Gael 20 3");
+						}
+						phase++;
 					}
-					phase++;
 					break;
-
 				case 4:
 					if (controller.choix == 1) {
-						for (int i = 0; i < controller.nombredeVotants; i++) {
+						for (int i = 1; i < controller.nombredeVotants; i++) {
 							while (true) {
 								try {
-									affiche("Quel est ton vote?(Si le vote est un mauvais nombre: Vote blanc)\n"
+									affiche("Quel est ton vote?(Si le vote est un mauvais nombre: Vote blanc)"
 											+ controller.str);
-									int c = Integer.parseInt(textField.getText());
-									controller.vote(c, i);
+									int d = Integer.parseInt(textField.getText());
+									controller.vote(d, i);
 									break;
-								} catch (InputMismatchException e) {
+								} catch (InputMismatchException d) {
 									affiche("Entrez un nombre s'il vous plait!\n");
 									continue;
 								}
@@ -133,12 +140,12 @@ public class VoteVueGUI extends VoteVue {
 									affiche("Donne ton nom,ton age et ton vote espacÃ©, ex:'Gael 20 3");
 									String s = textField.getText();
 									String decoupe[] = s.split(" ");
-									String c = decoupe[0];
+									String d = decoupe[0];
 									int k = Integer.parseInt(decoupe[1]);
 									int j = Integer.parseInt(decoupe[2]);
-									controller.votantComplet(c, k, j, i);
+									controller.votantComplet(d, k, j, i);
 									break;
-								} catch (InputMismatchException e) {
+								} catch (InputMismatchException d) {
 									affiche("Entrez un nombre s'il vous plait!");
 									continue;
 								}
@@ -152,7 +159,7 @@ public class VoteVueGUI extends VoteVue {
 				}
 			}
 		});
-		valider.setBounds(374, 0, 89, 23);
+		valider.setBounds(449, 0, 145, 23);
 		voteJFrame.getContentPane().add(valider);
 	}
 
