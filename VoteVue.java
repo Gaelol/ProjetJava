b587b71eec434498b7ff633dc82c7542e1223d5f
Gaelol <37298427+Gaelol@users.noreply.github.com>
2018-12-21 17:13:53 +0100

    Update VoteVue.java

--- VoteVue.java
+++ VoteVue.java
@@ -1,19 +1,31 @@
-package ProjetJava;
+package ProjetJava.view;
 /**
  * @author De Mal Raphaël, Dieuzeide Gaël
  */
 import java.util.Observer;
 
-public abstract class VoteVue implements Observer {
+import ProjetJava.controller.VoteController;
+import ProjetJava.model.Vote;
 
+public abstract class VoteVue implements Observer {
+	/**
+	 * the view is linked with model and the controller
+	 */
 	protected Vote model;
 	protected VoteController controller;
-
+/**
+ * Create the view  and add an observer on the model and controller
+ * @param model
+ * @param controller
+ */
 	VoteVue(Vote model, VoteController controller) {
 		this.model = model;
 		this.controller = controller;
 		model.addObserver(this);
 	}
-
+/**
+ * Create an abstract method affiche
+ * @param string
+ */
 	public abstract void affiche(String string);
 }
